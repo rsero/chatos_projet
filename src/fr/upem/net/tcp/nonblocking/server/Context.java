5c2a1ca8eb1a71f792cc21a8e6076f58ae0fee26
mamaru1 <mamaru@etud.u-pem.fr>
2021-03-15 10:28:45 +0100

    repertoire reader

--- src/fr/upem/net/tcp/nonblocking/server/Context.java
+++ src/fr/upem/net/tcp/nonblocking/server/Context.java
@@ -1,14 +1,92 @@
 package fr.upem.net.tcp.nonblocking.server;
 
+import fr.upem.net.tcp.nonblocking.server.reader.Reader;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
 
 public class Context {
-
+    private static int BUFFER_SIZE = 1024;
     private final ServerChatos server;
     private final SelectionKey key;
+    private final SocketChannel sc;
+    private boolean closed = false;
+    private final ByteBuffer bbin = ByteBuffer.allocate(BUFFER_SIZE);
+    private final ByteBuffer bbout = ByteBuffer.allocate(BUFFER_SIZE);
 
     public Context(ServerChatos server, SelectionKey key) {
         this.server=server;
         this.key=key;
+        this.sc = (SocketChannel) key.channel();
     }
+    private void updateInterestOps() {
+        int newInterestOps = 0;
+        if (!closed && bbin.hasRemaining()) {
+            newInterestOps = newInterestOps | SelectionKey.OP_READ;
+        }
+
+        if (bbout.position() > 0) {
+            newInterestOps = newInterestOps | SelectionKey.OP_WRITE;
+        }
+        if (newInterestOps == 0) {
+            silentlyClose();
+        }
+        key.interestOps(newInterestOps);
+    }
+
+    private void silentlyClose() {
+        try {
+            sc.close();
+        } catch (IOException e) {
+            // ignore exception
+        }
+    }
+
+    public void doRead() throws IOException {
+        if (sc.read(bbin) == -1) {
+            closed = true;
+        }
+        processIn();
+        updateInterestOps();
+    }
+
+    public void doWrite() throws IOException {
+        bbout.flip();
+        sc.write(bbout);
+        bbout.compact();
+        processOut();
+        updateInterestOps();
+    }
+
+    private void processIn() {
+//        for(;;) {
+//            //Reader.ProcessStatus status = mr.process(bbin);
+//            switch(status) {
+//                case DONE:
+//                    //Message msg = mr.get();
+//                    //server.broadcast(msg);
+//                    //mr.reset();
+//                    break;
+//                case REFILL:
+//                    return;
+//                case ERROR:
+//                    silentlyClose();
+//                    return;
+//            }
+//        }
+    }
+
+    private void processOut() {
+//        while (!queue.isEmpty()) {
+//            var msg = queue.remove();
+//            bbout.putInt(UTF8.encode(msg.getLogin()).remaining());
+//            bbout.put(UTF8.encode(msg.getLogin()));
+//            bbout.putInt(UTF8.encode(msg.getMsg()).remaining());
+//            bbout.put(UTF8.encode(msg.getMsg()));
+//        }
+    }
+
+
 }
--- src/fr/upem/net/tcp/nonblocking/server/ServerChatos.java
+++ src/fr/upem/net/tcp/nonblocking/server/ServerChatos.java
@@ -1,9 +1,83 @@
 package fr.upem.net.tcp.nonblocking.server;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channel;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerChatos {
     private final HashMap<String, SelectionKey> clients = new HashMap<>();
+    private final ServerSocketChannel serverSocketChannel;
+    private final Selector selector;
+    private static Logger logger = Logger.getLogger(ServerChatos.class.getName());
+
+    public ServerChatos() throws IOException {
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(7777));
+        selector = Selector.open();
+    }
+
+
+    public void launch() throws IOException {
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (!Thread.interrupted()) {
+            System.out.println("Starting select");
+            try {
+                selector.select(this::treatKey);
+            } catch (UncheckedIOException tunneled) {
+                throw tunneled.getCause();
+            }
+            System.out.println("Select finished");
+        }
+    }
+
+    private void treatKey(SelectionKey key) {
+        try {
+            if (key.isValid() && key.isAcceptable()) {
+                doAccept(key);
+            }
+        } catch (IOException ioe) {
+            // lambda call in select requires to tunnel IOException
+            throw new UncheckedIOException(ioe);
+        }
+        try {
+            if (key.isValid() && key.isWritable()) {
+                ((Context) key.attachment()).doWrite();
+            }
+            if (key.isValid() && key.isReadable()) {
+                ((Context) key.attachment()).doRead();
+            }
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Connection closed with client due to IOException", e);
+            silentlyClose(key);
+        }
+    }
+
+    private void doAccept(SelectionKey key) throws IOException {
+        var client = serverSocketChannel.accept();
+        if (client == null) {
+            logger.warning("The selector was wrong");
+            return;
+        }
+        client.configureBlocking(false);
+        var clientKey = client.register(selector, SelectionKey.OP_READ);
+        clientKey.attach(new Context(this, clientKey));
+    }
+
+    private void silentlyClose(SelectionKey key) {
+        Channel sc = (Channel) key.channel();
+        try {
+            sc.close();
+        } catch (IOException e) {
+            // ignore exception
+        }
+    }
 
 }
