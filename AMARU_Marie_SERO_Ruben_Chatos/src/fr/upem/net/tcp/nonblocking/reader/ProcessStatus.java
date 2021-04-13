c72eaa3906fd65f50a5f3f344d2c49dca665dccd
mamaru1 <mamaru@etud.u-pem.fr>
2021-04-13 19:41:02 +0200

    rendu 1

--- AMARU_Marie_SERO_Ruben_Chatos/src/fr/upem/net/tcp/nonblocking/reader/ProcessStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.upem.net.tcp.nonblocking.reader;
-
-public enum ProcessStatus {
-	DONE,REFILL,ERROR
-}
--- projet_avant/src/fr/upem/net/tcp/nonblocking/server/reader/ProcessStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.upem.net.tcp.nonblocking.server.reader;
-
-public enum ProcessStatus {
-	DONE,REFILL,ERROR
-}
